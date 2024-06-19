11b2ab5771678368ea56d8ea230c17ae52be5a7b
AmirAsefiDev <amirasefi.info@gmail.com>
2024-06-19 16:04:50 +0330

    Add Convertor

---
+++ DbClassGenerator/MainForm.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace DbClassGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataSourceLabel = new System.Windows.Forms.Label();
+            this.DataSourceTextBox = new System.Windows.Forms.TextBox();
+            this.UserIdCheckBox = new System.Windows.Forms.CheckBox();
+            this.UserIdTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.Connect = new System.Windows.Forms.Button();
+            this.DatabasesComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TablesCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RootNameSpaceTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // DataSourceLabel
+            // 
+            this.DataSourceLabel.AutoSize = true;
+            this.DataSourceLabel.Location = new System.Drawing.Point(13, 10);
+            this.DataSourceLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DataSourceLabel.Name = "DataSourceLabel";
+            this.DataSourceLabel.Size = new System.Drawing.Size(72, 15);
+            this.DataSourceLabel.TabIndex = 0;
+            this.DataSourceLabel.Text = "Data Source";
+            // 
+            // DataSourceTextBox
+            // 
+            this.DataSourceTextBox.Location = new System.Drawing.Point(93, 7);
+            this.DataSourceTextBox.Name = "DataSourceTextBox";
+            this.DataSourceTextBox.Size = new System.Drawing.Size(185, 23);
+            this.DataSourceTextBox.TabIndex = 1;
+            // 
+            // UserIdCheckBox
+            // 
+            this.UserIdCheckBox.AutoSize = true;
+            this.UserIdCheckBox.Location = new System.Drawing.Point(282, 9);
+            this.UserIdCheckBox.Name = "UserIdCheckBox";
+            this.UserIdCheckBox.Size = new System.Drawing.Size(65, 19);
+            this.UserIdCheckBox.TabIndex = 2;
+            this.UserIdCheckBox.Text = "User ID";
+            this.UserIdCheckBox.UseVisualStyleBackColor = true;
+            this.UserIdCheckBox.CheckedChanged += new System.EventHandler(this.UserIdCheckBox_CheckedChanged);
+            // 
+            // UserIdTextBox
+            // 
+            this.UserIdTextBox.Enabled = false;
+            this.UserIdTextBox.Location = new System.Drawing.Point(349, 7);
+            this.UserIdTextBox.Name = "UserIdTextBox";
+            this.UserIdTextBox.Size = new System.Drawing.Size(185, 23);
+            this.UserIdTextBox.TabIndex = 3;
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(541, 11);
+            this.PasswordLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(57, 15);
+            this.PasswordLabel.TabIndex = 4;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Enabled = false;
+            this.PasswordTextBox.Location = new System.Drawing.Point(605, 8);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.Size = new System.Drawing.Size(185, 23);
+            this.PasswordTextBox.TabIndex = 5;
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(809, 7);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(75, 23);
+            this.Connect.TabIndex = 6;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // DatabasesComboBox
+            // 
+            this.DatabasesComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DatabasesComboBox.Enabled = false;
+            this.DatabasesComboBox.FormattingEnabled = true;
+            this.DatabasesComboBox.Location = new System.Drawing.Point(93, 51);
+            this.DatabasesComboBox.Name = "DatabasesComboBox";
+            this.DatabasesComboBox.Size = new System.Drawing.Size(791, 23);
+            this.DatabasesComboBox.TabIndex = 7;
+            this.DatabasesComboBox.SelectedIndexChanged += new System.EventHandler(this.DatabasesComboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Data Source";
+            // 
+            // TablesCheckedListBox
+            // 
+            this.TablesCheckedListBox.FormattingEnabled = true;
+            this.TablesCheckedListBox.Location = new System.Drawing.Point(12, 80);
+            this.TablesCheckedListBox.Name = "TablesCheckedListBox";
+            this.TablesCheckedListBox.Size = new System.Drawing.Size(872, 310);
+            this.TablesCheckedListBox.TabIndex = 9;
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(796, 396);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(88, 31);
+            this.GenerateButton.TabIndex = 10;
+            this.GenerateButton.Text = "Generate...";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 404);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Root Namespace";
+            // 
+            // RootNameSpaceTextBox
+            // 
+            this.RootNameSpaceTextBox.Location = new System.Drawing.Point(112, 401);
+            this.RootNameSpaceTextBox.Name = "RootNameSpaceTextBox";
+            this.RootNameSpaceTextBox.Size = new System.Drawing.Size(678, 23);
+            this.RootNameSpaceTextBox.TabIndex = 12;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 484);
+            this.Controls.Add(this.RootNameSpaceTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.GenerateButton);
+            this.Controls.Add(this.TablesCheckedListBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DatabasesComboBox);
+            this.Controls.Add(this.Connect);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UserIdTextBox);
+            this.Controls.Add(this.UserIdCheckBox);
+            this.Controls.Add(this.DataSourceTextBox);
+            this.Controls.Add(this.DataSourceLabel);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Database .NET Classes Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DataSourceLabel;
+        private System.Windows.Forms.TextBox DataSourceTextBox;
+        private System.Windows.Forms.CheckBox UserIdCheckBox;
+        private System.Windows.Forms.TextBox UserIdTextBox;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.ComboBox DatabasesComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox TablesCheckedListBox;
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox RootNameSpaceTextBox;
+    }
+}
+
---
+++ DbClassGenerator/MainForm.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DbClassGenerator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UserIdCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            UserIdTextBox.Enabled = PasswordTextBox.Enabled = UserIdCheckBox.Checked;
+            if (UserIdCheckBox.Checked)
+                UserIdCheckBox.Focus();
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            using (var connection = new SqlConnection(GetConnectionString("master")))
+            {
+                connection.Open();
+                var command = new SqlCommand(" SELECT * FROM sys.databases", connection);
+                var reader = command.ExecuteReader();
+                DatabasesComboBox.Items.Clear();
+                while (reader.Read())
+                {
+                    DatabasesComboBox.Items.Add(reader["name"]);
+                }
+                DatabasesComboBox.Enabled = true;
+            }
+        }
+
+        private string GetConnectionString(string databaseName)
+        {
+            var connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = DataSourceTextBox.Text;
+            connectionStringBuilder.InitialCatalog = databaseName;
+            connectionStringBuilder.MultipleActiveResultSets = true;
+            if (UserIdCheckBox.Checked)
+            {
+                connectionStringBuilder.IntegratedSecurity = false;
+                connectionStringBuilder.UserID = UserIdTextBox.Text;
+                connectionStringBuilder.Password = PasswordTextBox.Text;
+            }
+            else
+                connectionStringBuilder.IntegratedSecurity = true;
+            return connectionStringBuilder.ConnectionString;
+        }
+
+        private void DatabasesComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var connectionString = GetConnectionString(DatabasesComboBox.SelectedItem.ToString());
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT * FROM INFORMATION_SCHEMA.TABLES", connection);
+                var reader = command.ExecuteReader();
+                TablesCheckedListBox.Items.Clear();
+                while (reader.Read())
+                {
+                    var schema = reader["TABLE_SCHEMA"];
+                    var table = reader["TABLE_NAME"];
+                    TablesCheckedListBox.Items.Add(schema + "." + table);
+                }
+            }
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            var folderBrowser = new FolderBrowserDialog();
+            if (folderBrowser.ShowDialog() != DialogResult.OK)
+                return;
+
+            foreach (var item in TablesCheckedListBox.CheckedItems)
+            {
+                var text = item.ToString();
+                var schema = text.Split('.')[0];
+                var table = text.Split('.')[1];
+                var columns = GetTableColumns(schema, table);
+
+                GenerateEntities(folderBrowser.SelectedPath, schema, table, columns);
+                GenerateRepositoryInterfaces(folderBrowser.SelectedPath , schema , table , columns );
+                GenerateRepositories(folderBrowser.SelectedPath , schema , table , columns );
+            }
+        }
+
+        private void GenerateRepositories(string generatePath, string schema, string table, List<ColumnModel> columns)
+        {
+            var entitiesFolder = Path.Combine(generatePath, "Repositories");
+            if (!Directory.Exists(entitiesFolder))
+                Directory.CreateDirectory(entitiesFolder);
+
+            List<string> classLines = new List<String>();
+            classLines.Add("using System;");
+            classLines.Add("using System.Collections.Generic;");
+            classLines.Add("using System.Data.SqlClient;");
+            classLines.Add("");
+            classLines.Add("namespace " + RootNameSpaceTextBox.Text + ".Repositories");
+            classLines.Add("{");
+            classLines.Add("    public class " + table + "Repository : DataLayer.GenericRepository<Entities." + GetSingularName(table) + ">,RepositoryAbstracts.I"+ table +"Repository");
+            classLines.Add("    {");
+            classLines.Add("        public "+ table +"Repository() : base(\"name=DbConnectionString\") { }");
+            foreach (var column in columns)
+            {
+                var dataType = ConvertSqlTypeClr(column.DataType, column.IsNUllable);
+                classLines.Add("        public List<Entities."+ GetSingularName(table) +"> GetBy"+column.Name + "(" + ConvertSqlTypeClr(column.DataType, column.IsNUllable) + " value)");
+                classLines.Add("        {");
+                if (dataType == "string")
+                {
+                    classLines.Add("            return RunQuery(\"SELECT * FROM ["+ schema +"].[" + table + "] WHERE [" + column.Name + "] LIKE @Value\", new SqlParameter(\"Value\", value));");
+                }
+                else
+                {
+                    classLines.Add("            return RunQuery(\"SELECT * FROM ["+ schema +"].[" + table + "] WHERE [" + column.Name + "] = @Value\", new SqlParameter(\"Value\", value));");
+                }
+                //classLines.Add("        List<Entities." + GetSingularName(table) + "> GetBy" + column.Name + "(" + ConvertSqlTypeClr(column.DataType, column.IsNUllable) + " value);");
+                classLines.Add("        }");
+            }
+            classLines.Add("    }");
+            classLines.Add("}");
+
+            File.WriteAllLines(Path.Combine(entitiesFolder, GetSingularName(table) + ".cs"), classLines);
+        }
+
+        private void GenerateRepositoryInterfaces(string generatePath, string schema, string table, List<ColumnModel> columns)
+        {
+            var entitiesFolder = Path.Combine(generatePath, "Abstracts");
+            if (!Directory.Exists(entitiesFolder))
+                Directory.CreateDirectory(entitiesFolder);
+
+            List<string> classLines = new List<String>();
+            classLines.Add("using System;");
+            classLines.Add("using System.Collections.Generic;");
+            classLines.Add("");
+            classLines.Add("namespace " + RootNameSpaceTextBox.Text + ".RepositoryAbstracts");
+            classLines.Add("{");
+            classLines.Add("    public interface I" + table + "Repository : DataLayer.IRepository<Entities." + GetSingularName(table) + ">");
+            classLines.Add("    {");
+
+            foreach (var column in columns)
+            {
+                classLines.Add("        List<Entities."+ GetSingularName(table) +"> GetBy"+ column.Name + "(" + ConvertSqlTypeClr(column.DataType, column.IsNUllable)+ " value);");
+            }
+            classLines.Add("    }");
+            classLines.Add("}");
+
+            File.WriteAllLines(Path.Combine(entitiesFolder, GetSingularName(table) + ".cs"), classLines);
+        }
+
+        private void GenerateEntities(string generatePath, string schema, string table, List<ColumnModel> columns)   
+        {
+            var entitiesFolder = Path.Combine(generatePath, "Entities");
+            if (!Directory.Exists(entitiesFolder))
+                Directory.CreateDirectory(entitiesFolder);
+            List<string> classLines = new List<String>();
+            classLines.Add("using System;");
+            classLines.Add("");
+            classLines.Add("namespace " + RootNameSpaceTextBox.Text + ".Entities");
+            classLines.Add("{");
+            classLines.Add("    [DataLayer.Table(\"" + schema + "\",\"" + table + "\")]");
+            classLines.Add("    public class " + GetSingularName(table));
+            classLines.Add("    {");
+
+            foreach (var column in columns)
+            {
+                if (column.IsPriamaryKey)
+                {
+                    classLines.Add("        [DataLayer.PrimaryKey]");
+                }
+                if (column.IsComputed)
+                {
+                    classLines.Add("        [DataLayer.ComputedColumn]");
+                }
+                classLines.Add("        public " + ConvertSqlTypeClr(column.DataType, column.IsNUllable) + " " + column.Name + " { get; set; }");
+            }
+            classLines.Add("    }");
+            classLines.Add("}");
+
+            File.WriteAllLines(Path.Combine(entitiesFolder, GetSingularName(table) + ".cs"), classLines);
+        }
+
+        private string ConvertSqlTypeClr(string type, bool nullable)
+        {
+            switch (type)
+            {
+                case "int":
+                    return nullable ? "int?" : "int";
+
+                case "bigint":
+                    return nullable ? "long?" : "long";
+
+                case "datetime":
+                case "date":
+                case "datetime2":
+                    return nullable ? "DateTime?" : "DateTime";
+
+                case "nvarchar":
+                case "nchar":
+                case "varchar":
+                case "char":
+                    return "string";
+
+                case "bit":
+                    return nullable ? "bool?" : "bool";
+
+                case "binary":
+                case "image":
+                    return "byte[]";
+
+                case "decimal":
+                    return nullable ? "decimal?" : "decimal";
+
+                case "float":
+                    return nullable ? "float?" : "float";
+            }
+            return "object";
+        }
+
+        private string GetSingularName(string name)
+        {
+            if (name.EndsWith("ies"))
+            {
+                return name.Substring(0, name.Length - 3) + "y";
+            }
+            return name.Substring(0, name.Length - 1);
+        }
+
+        private List<ColumnModel> GetTableColumns(string schema, string tableName)
+        {
+            var columns = new List<ColumnModel>();
+
+            using (var connection = new SqlConnection(GetConnectionString(DatabasesComboBox.SelectedItem.ToString())))
+            {
+                connection.Open();
+                List<string> primaryKeyColumns = new List<string>();
+                var keysCommand = new SqlCommand("SELECT tc.TABLE_SCHEMA , tc.TABLE_NAME , ccu.COLUMN_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc inner Join INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE ccu on ccu.CONSTRAINT_NAME = tc.CONSTRAINT_NAME WHERE tc.TABLE_SCHEMA = N'" + schema + "' AND tc.TABLE_NAME = N'" + tableName + "' AND tc.CONSTRAINT_TYPE = N'PRIMARY KEY'", connection);
+                var KeysReader = keysCommand.ExecuteReader();
+                while (KeysReader.Read())
+                {
+                    primaryKeyColumns.Add(KeysReader["COLUMN_NAME"].ToString());
+                }
+                List<string> computedColumns = new List<string>();
+                var computedCommand = new SqlCommand("SELECT [name] FROM sys.columns WHERE object_id = object_id('" + schema + "." + tableName + "') AND (is_identity = 1 OR is_computed = 1)", connection);
+                var computedReader = computedCommand.ExecuteReader();
+                while (computedReader.Read())
+                {
+                    computedColumns.Add(computedReader["name"].ToString());
+                }
+                var command = new SqlCommand("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = N'" + schema + "' AND TABLE_NAME = N'" + tableName + "'", connection);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var columnModel = new ColumnModel()
+                    {
+                        IsPriamaryKey = primaryKeyColumns.Any(col => col.Equals(reader["COLUMN_NAME"])),
+                        IsComputed = computedColumns.Any(col => col.Equals(reader["COLUMN_NAME"])),
+                        Name = reader["COLUMN_NAME"].ToString(),
+                        DataType = reader["DATA_TYPE"].ToString(),
+                        IsNUllable = reader["IS_NULLABLE"].ToString() == "YES"
+                    };
+                    columns.Add(columnModel);
+                }
+            }
+
+            return columns;
+        }
+    }
+    public class ColumnModel
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+
+        public bool IsPriamaryKey { get; set; }
+        public bool IsComputed { get; set; }
+        public bool IsNUllable { get; set; }
+    }
+}
